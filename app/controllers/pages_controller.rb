38c9f179f60f3dbcfd7956e48bae4a0bd92764c8
Namazouchan <namazou.challenger@gmail.com>
2024-04-25 23:13:18 +0900

    sidebarcomplete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
   def complete
   end
+
+  def feedback
+  end
+
+  def goalset
+  end
+  
+  def progress
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'home', to: 'pages#home'
   get 'complete', to: 'pages#complete'
   get 'feedback', to: 'pages#feedback'
   get 'goalset', to: 'pages#goalset'
