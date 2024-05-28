3fabbdb73f4270c972b4837f9aedfabace015c62
Namazouchan <namazou.challenger@gmail.com>
2024-05-29 08:48:14 +0900

    postリクエスト完成これから認証

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
       private
     
       def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:name, :password)
       end
       
 end
