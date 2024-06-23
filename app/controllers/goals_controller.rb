665a2a8d38c0421737f96e6c25645b0d9a0e1f27
Namazouchan <namazou.challenger@gmail.com>
2024-06-23 22:49:46 +0900

    複数登録できたが、全て1が保存されてしまう要修正

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -15,18 +15,24 @@ class GoalsController < ApplicationController
   
   def create
     @long_term_goal = LongTermGoal.new(long_term_goal_params)
-    @mid_term_goal = MidTermGoal.new(mid_term_goal_params)
-    
-    # ユーザー情報を設定
+    mid_term_goals = []
+    mid_term_goals << MidTermGoal.new(mid_term_goal_params) if params[:goal][:mid_goal].present?
+    mid_term_goals << MidTermGoal.new(mid_term_goal_params_2) if params[:goal][:mid_goal_2].present?
+    mid_term_goals << MidTermGoal.new(mid_term_goal_params_3) if params[:goal][:mid_goal_3].present?
+    mid_term_goals << MidTermGoal.new(mid_term_goal_params_4) if params[:goal][:mid_goal_4].present?
+    mid_term_goals << MidTermGoal.new(mid_term_goal_params_5) if params[:goal][:mid_goal_5].present?
+
     @long_term_goal.user = current_user
-    @mid_term_goal.user = current_user
-    @mid_term_goal.long_term_goal = @long_term_goal
+    mid_term_goals.each do |mid_term_goal|
+      mid_term_goal.user = current_user
+      mid_term_goal.long_term_goal = @long_term_goal
+    end
 
     if logged_in?
       # トランザクションで保存
       ActiveRecord::Base.transaction do
         @long_term_goal.save!
-        @mid_term_goal.save!
+        mid_term_goals.each(&:save!)
       end
       redirect_to goals_path, notice: '目標が作成されました。'
     else
@@ -38,33 +44,25 @@ class GoalsController < ApplicationController
     render :new
   end
   
-    def edit
-    end
-  
-    def update
-      if @goal.update(goal_params)
-        redirect_to @goal, notice: '目標が更新されました。'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @goal.destroy
-      redirect_to goals_path, notice: '目標が削除されました。'
-    end
-  
     private
-  
-    def set_goal
-      @goal = Goal.find(params[:id])
-    end
 
     def long_term_goal_params
       params.require(:goal).permit(:long_goal, :long_goal_deadline)
     end
     
     def mid_term_goal_params
+      params.require(:goal).permit(:mid_goal, :what_to_do, :why_to_do, :current_status, :why_current_status, :what_next, :mid_goal_deadline, :priority)
+    end
+    def mid_term_goal_params_2
+      params.require(:goal).permit(:mid_goal, :what_to_do, :why_to_do, :current_status, :why_current_status, :what_next, :mid_goal_deadline, :priority)
+    end
+    def mid_term_goal_params_3
+      params.require(:goal).permit(:mid_goal, :what_to_do, :why_to_do, :current_status, :why_current_status, :what_next, :mid_goal_deadline, :priority)
+    end
+    def mid_term_goal_params_4
+      params.require(:goal).permit(:mid_goal, :what_to_do, :why_to_do, :current_status, :why_current_status, :what_next, :mid_goal_deadline, :priority)
+    end
+    def mid_term_goal_params_5
       params.require(:goal).permit(:mid_goal, :what_to_do, :why_to_do, :current_status, :why_current_status, :what_next, :mid_goal_deadline, :priority)
     end
 end
