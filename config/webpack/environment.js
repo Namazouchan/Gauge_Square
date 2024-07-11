2aad1c6b10701214e882c232dd2b06c686a8e6be
Namazouchan <namazou.challenger@gmail.com>
2024-07-12 00:57:45 +0900

    entry add

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+module.exports = (env) => {
+  const envConfig = require(`./${env}`);
+  return merge(commonConfig, envConfig, {
+    entry: './app/javasctipt/controllers/goals.js', // エントリーポイントを共通設定に追加
+  });
+};
+
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
