375c457e307319066c41c1cf216d13f992c2ca66
Namazouchan <namazou.challenger@gmail.com>
2024-08-07 18:50:46 +0900

    grapf newedit

--- app/javascript/controllers/goals.js
+++ app/javascript/controllers/goals.js
@@ -90,3 +90,27 @@ document.addEventListener("turbo:load", () => {
   addRemoveEventListeners();
   console.log("Event listeners added to initial remove buttons.");
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar', // グラフのタイプ
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'Dataset 1',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+});
